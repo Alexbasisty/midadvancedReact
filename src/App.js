c21e34f86537944a2cd98e082682d9ca146cd4df
Alex Basystyi <alexbasisty@gmail.com>
2021-03-15 10:50:05 +0100

    styled-components: testing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,42 @@
-const App = () => <h1>Hello React World!!!</h1>;
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 
-export default App;
+const Button = styled.button`
+    background: ${(props) =>
+        props.secondary
+            ? props.theme.colors.secondary
+            : props.theme.colors.main};
+    color: ${(props) => (props.secondary ? "red" : "white")};
+    font-size: 1 em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid blue;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+`;
+
+const InlineButton = styled(Button)`
+    background: unset;
+    color: black;
+`;
+
+const theme = {
+    colors: {
+        main: "red",
+        secondary: "white",
+    },
+};
+
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Button>Styled Component</Button>
+            <Button secondary>Styled Component: Secondary</Button>
+            <InlineButton>Inline Button</InlineButton>
+        </ThemeProvider>
+    );
+}
