d025f23ed1c90d62381861aeac8f58f69bf0ee05
Alex Basystyi <alexbasisty@gmail.com>
2021-03-16 18:16:04 +0100

    logger - simple redux middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const logger = (store) => (next) => (action) => {
+    console.group(action.type);
+    console.info("dispatching", action);
+    let result = next(action);
+    console.log("next state", store.getState());
+    console.groupEnd();
+
+    return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
     <React.StrictMode>
